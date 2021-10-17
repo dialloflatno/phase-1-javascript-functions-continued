ea35b856a12a8743e8715ac0fc6e5b628490656e
dialloflatno <brahimdiallo23@gmail.com>
2021-10-17 03:50:19 -0400

    commit tree 6a6883971b2a19dbb78129e1c9489cae1efd900d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+
+saturdayFun = (activity = 'roller-skate') => `This Saturday, I want to ${activity}!`;
+
+function mondayWork(activityWork = 'go to the office'){
+    return `This Monday, I will ${activityWork}.`;
+}
+
+
+function wrapAdjective(wrapper = "*") {
+    return function result(adjective = 'a hard worker') {
+        return `You are ${wrapper}${adjective}${wrapper}!`
+
+    }
+}
+
+
+
+
+
+
+
+
+
